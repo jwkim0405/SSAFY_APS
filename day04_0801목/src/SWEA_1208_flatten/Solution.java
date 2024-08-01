df1b5d7eb1d0df7cbedc996082ff0c4170460393
wjddn5193 <wjddn5193@naver.com>
2024-08-01 17:47:11 +0900

    0801 오후

--- day04_0801목/src/SWEA_1208_flatten/Solution.java
+++ day04_0801목/src/SWEA_1208_flatten/Solution.java
@@ -1,5 +1,34 @@
 package SWEA_1208_flatten;
 
-public class Solution {
+import java.util.Arrays;
+import java.util.Scanner;
 
+
+
+public class Solution {
+	static int[] height;
+	
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		// 총 10번의 Test Case
+		for (int T = 1; T <= 10; T++) 
+		{
+			int N = sc.nextInt();	// 덤프 횟수
+			
+			int[] height = new int[100]; // 가로 길이 100 배열
+			
+			for (int i = 0; i < 100; i++) 
+				height[i] = sc.nextInt(); // 배열 값 할당
+			
+			
+//			countingSort();
+			
+			System.out.println("#" + T + " ");
+		}
+	}
+	static void countingSort() {
+		
+	}
 }
--- day04_0801목/src/SWEA_2001_파리퇴치/Solution.java
+++ day04_0801목/src/SWEA_2001_파리퇴치/Solution.java
@@ -1,5 +1,50 @@
 package SWEA_2001_파리퇴치;
 
-public class Solution {
+import java.util.Scanner;
 
+public class Solution {
+	
+	static int N;
+	static int M;
+	static int[][] fly;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int testCase = sc.nextInt();
+		
+		for (int t = 1; t <= testCase; t++)
+		{
+			N = sc.nextInt();
+			M = sc.nextInt();
+			
+			fly = new int[N][N];
+			
+			for (int i = 0; i < N; i++)
+				for (int j = 0; j < N; j++)
+					fly[i][j] = sc.nextInt();
+			int n = sc.
+			int max = 0;
+			// 시작점 (i,j)의 사각형
+			for (int i = 0; i < N - M + 1; i++) {
+				for (int j = 0; j < N - M + 1; j++) {
+					int sum = sum(i,j);
+					if (max < sum)
+						max = sum;
+				}
+			}
+					
+			System.out.println("#" + t + " " + max);
+		}
+	}
+	
+	// MxM 크기의 사각형 합 구하는 method
+	static int sum(int i, int j) {
+		int sum = 0;
+		// M x M 크기의 값의 합
+		for (int p = 0; p < M; p++)
+			for (int q = 0; q < M; q++)
+				sum += fly[i+p][j+q]; 
+		
+		return sum;
+	}
 }
