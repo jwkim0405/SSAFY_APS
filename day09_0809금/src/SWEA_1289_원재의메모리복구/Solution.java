dccfa612c35c73d02760fec2bd2b6055da150a5d
jwkim0405 <jeongwoocode@gmail.com>
2024-08-11 22:13:23 +0900

    0811

--- day09_0809금/src/SWEA_1289_원재의메모리복구/Solution.java
+++ day09_0809금/src/SWEA_1289_원재의메모리복구/Solution.java
@@ -1,5 +1,38 @@
 package SWEA_1289_원재의메모리복구;
 
-public class Solution {
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class Solution {
+	static String memory;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int testCase = sc.nextInt();
+		
+		for (int t = 1; t <= testCase; t++)
+		{
+			memory = sc.next();
+			
+			//String을 char배열로 변환
+			char[] charArr = memory.toCharArray();
+			int N = charArr.length;
+			int cnt = 0;
+
+			if (charArr[0] == '1') 
+				cnt = 1;
+			 else 
+				 cnt = 0;
+			 
+				
+			for (int i = N-1; i >= 1; i--) 
+				if (charArr[i] != charArr[i-1])
+					cnt++;
+			
+			// 결과 출력
+			System.out.println("#" + t + " " + cnt);
+			
+		}
+	}
 }
